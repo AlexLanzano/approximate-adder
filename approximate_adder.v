589876773b649aeff593cb74f8e746bca1d2606e
Alex Lanzano <lanzano.alex@gmail.com>
2021-04-13 17:07:25 -0400

    Add more approximate adder implementations

--- approximate_adder.v
+++ approximate_adder.v
@@ -1,5 +1,42 @@
-module approximate_adder_1bit(input A, B, cin,
-                              output sum, cout);
+module inverter(input a,
+                output out);
+   supply1 vdd;
+   supply0 gnd;
+
+   pmos(out, vdd, a);
+   nmos(out, gnd, a);
+endmodule
+
+module approximate_adder_1bit_v1(input A, B, cin,
+                                 output sum, cout);
+   supply1 vdd;
+   supply0 gnd;
+
+   wire invert_cout, invert_sum;
+   inverter invert1(invert_cout, cout);
+   inverter invert2(invert_sum, sum);
+
+   pmos P1(a, vdd, B);
+   pmos P2(b, vdd, B);
+   pmos P3(invert_cout, a, cin);
+   pmos P4(invert_cout, b, A);
+   pmos P5(e, vdd, A);
+   pmos P6(e, vdd, B);
+   pmos P7(invert_sum, e, invert_cout);
+   pmos P8(invert_sum, vdd, cin);
+
+   nmos N1(c, gnd, A);
+   nmos N2(invert_cout, gnd, B);
+   nmos N3(invert_cout, c, cin);
+   nmos N4(d, gnd, cin);
+   nmos N5(invert_sum, d, invert_cout);
+   nmos N6(g, gnd, B);
+   nmos N7(f, g, A);
+   nmos N8(invert_sum, f, cin);
+endmodule
+
+module approximate_adder_1bit_v2(input A, B, cin,
+                                 output sum, cout);
    supply1 vdd;
    supply0 gnd;
 
@@ -25,43 +62,104 @@ module approximate_adder_1bit(input A, B, cin,
    
 endmodule
 
-module approximate_adder_8bit(input [7:0] A, B,
+module approximate_adder_1bit_v5(input A, B, cin,
+                                 output sum, cout);
+   assign sum = B;
+   assign cout = A;
+endmodule
+
+module approximate_adder_8bit_v1(input [7:0] A, B,
                               input cin,
                               output [7:0] sum,
                               output cout);
 
    wire [7:0] carry;
-   approximate_adder_1bit AA0(.A(A[0]), .B(B[0]), .cin(cin),
+   approximate_adder_1bit_v1 AA0(.A(A[0]), .B(B[0]), .cin(cin),
                               .sum(sum[0]), .cout(carry[0]));
 
-   approximate_adder_1bit AA1(.A(A[1]), .B(B[1]), .cin(carry[0]),
+   approximate_adder_1bit_v1 AA1(.A(A[1]), .B(B[1]), .cin(carry[0]),
                               .sum(sum[1]), .cout(carry[1]));
    
-   approximate_adder_1bit AA2(.A(A[2]), .B(B[2]), .cin(carry[1]),
+   approximate_adder_1bit_v1 AA2(.A(A[2]), .B(B[2]), .cin(carry[1]),
                               .sum(sum[2]), .cout(carry[2]));
    
-   approximate_adder_1bit AA3(.A(A[3]), .B(B[3]), .cin(carry[2]),
+   approximate_adder_1bit_v1 AA3(.A(A[3]), .B(B[3]), .cin(carry[2]),
                               .sum(sum[3]), .cout(carry[3]));
    
-   approximate_adder_1bit AA4(.A(A[4]), .B(B[4]), .cin(carry[3]),
+   approximate_adder_1bit_v1 AA4(.A(A[4]), .B(B[4]), .cin(carry[3]),
                               .sum(sum[4]), .cout(carry[4]));
    
-   approximate_adder_1bit AA5(.A(A[5]), .B(B[5]), .cin(carry[4]),
+   approximate_adder_1bit_v1 AA5(.A(A[5]), .B(B[5]), .cin(carry[4]),
                               .sum(sum[5]), .cout(carry[5]));
    
-   approximate_adder_1bit AA6(.A(A[6]), .B(B[6]), .cin(carry[5]),
+   approximate_adder_1bit_v1 AA6(.A(A[6]), .B(B[6]), .cin(carry[5]),
                               .sum(sum[6]), .cout(carry[6]));
    
-   approximate_adder_1bit AA7(.A(A[7]), .B(B[7]), .cin(carry[6]),
+   approximate_adder_1bit_v1 AA7(.A(A[7]), .B(B[7]), .cin(carry[6]),
                               .sum(sum[7]), .cout(carry[7]));
    assign cout = cin^carry[7];
 endmodule
 
-module inverter(input a,
-                output out);
-   supply1 vdd;
-   supply0 gnd;
+module approximate_adder_8bit_v2(input [7:0] A, B,
+                              input cin,
+                              output [7:0] sum,
+                              output cout);
 
-   pmos(out, vdd, a);
-   nmos(out, gnd, a);
+   wire [7:0] carry;
+   approximate_adder_1bit_v2 AA0(.A(A[0]), .B(B[0]), .cin(cin),
+                              .sum(sum[0]), .cout(carry[0]));
+
+   approximate_adder_1bit_v2 AA1(.A(A[1]), .B(B[1]), .cin(carry[0]),
+                              .sum(sum[1]), .cout(carry[1]));
+   
+   approximate_adder_1bit_v2 AA2(.A(A[2]), .B(B[2]), .cin(carry[1]),
+                              .sum(sum[2]), .cout(carry[2]));
+   
+   approximate_adder_1bit_v2 AA3(.A(A[3]), .B(B[3]), .cin(carry[2]),
+                              .sum(sum[3]), .cout(carry[3]));
+   
+   approximate_adder_1bit_v2 AA4(.A(A[4]), .B(B[4]), .cin(carry[3]),
+                              .sum(sum[4]), .cout(carry[4]));
+   
+   approximate_adder_1bit_v2 AA5(.A(A[5]), .B(B[5]), .cin(carry[4]),
+                              .sum(sum[5]), .cout(carry[5]));
+   
+   approximate_adder_1bit_v2 AA6(.A(A[6]), .B(B[6]), .cin(carry[5]),
+                              .sum(sum[6]), .cout(carry[6]));
+   
+   approximate_adder_1bit_v2 AA7(.A(A[7]), .B(B[7]), .cin(carry[6]),
+                              .sum(sum[7]), .cout(carry[7]));
+   assign cout = cin^carry[7];
+endmodule
+
+module approximate_adder_8bit_v5(input [7:0] A, B,
+                              input cin,
+                              output [7:0] sum,
+                              output cout);
+
+   wire [7:0] carry;
+   approximate_adder_1bit_v5 AA0(.A(A[0]), .B(B[0]), .cin(cin),
+                              .sum(sum[0]), .cout(carry[0]));
+
+   approximate_adder_1bit_v5 AA1(.A(A[1]), .B(B[1]), .cin(carry[0]),
+                              .sum(sum[1]), .cout(carry[1]));
+   
+   approximate_adder_1bit_v5 AA2(.A(A[2]), .B(B[2]), .cin(carry[1]),
+                              .sum(sum[2]), .cout(carry[2]));
+   
+   approximate_adder_1bit_v5 AA3(.A(A[3]), .B(B[3]), .cin(carry[2]),
+                              .sum(sum[3]), .cout(carry[3]));
+   
+   approximate_adder_1bit_v5 AA4(.A(A[4]), .B(B[4]), .cin(carry[3]),
+                              .sum(sum[4]), .cout(carry[4]));
+   
+   approximate_adder_1bit_v5 AA5(.A(A[5]), .B(B[5]), .cin(carry[4]),
+                              .sum(sum[5]), .cout(carry[5]));
+   
+   approximate_adder_1bit_v5 AA6(.A(A[6]), .B(B[6]), .cin(carry[5]),
+                              .sum(sum[6]), .cout(carry[6]));
+   
+   approximate_adder_1bit_v5 AA7(.A(A[7]), .B(B[7]), .cin(carry[6]),
+                              .sum(sum[7]), .cout(carry[7]));
+   assign cout = cin^carry[7];
 endmodule
--- approximate_adder_tb.v
+++ approximate_adder_tb.v
@@ -1,3 +1,30 @@
+// module truth_table_v1();
+//    integer i;
+   
+//    reg A, B, cin;
+
+//    wire A_tv = A;
+//    wire B_tv = B;
+//    wire cin_tv = cin;
+
+//    wire sum_ov;
+//    wire cout_ov;
+
+//    approximate_adder_1bit_v5 UUT(.A(A_tv), .B(B_tv), .cin(cin_tv),
+//                                  .sum(sum_ov), .cout(cout_ov));
+   
+//    initial begin : truthtablev1
+//       $display("| A | B | cin | sum | cout |");
+//       for (i = 0; i < 8; i = i + 1) begin
+//          {A, B, cin} = i;
+//          #1;
+//          $display("| %d | %d |  %d  |  %d  |   %d  |", A_tv, B_tv, cin_tv, sum_ov, cout_ov);
+//       end
+//    end
+   
+// endmodule
+
+   
 module approximate_adder_8bit_tb();
    integer i = 0;
    reg [7:0] a, b, expected_sum;
@@ -7,40 +34,67 @@ module approximate_adder_8bit_tb();
    wire [7:0] b_tv = b[7:0];
    wire cin_tv = 0;
 
-   wire [7:0] sum_ov;
-   wire cout_ov;
+   wire [7:0] sum_ov_v1;
+   wire cout_ov_v1;
 
-   approximate_adder_8bit UUT(.A(a_tv), .B(b_tv), 
-                              .cin(cin_tv),
-                              .sum(sum_ov),
-                              .cout(cout_ov));
+   wire [7:0] sum_ov_v2;
+   wire cout_ov_v2;
 
+   wire [7:0] sum_ov_v5;
+   wire cout_ov_v5;
+
+   approximate_adder_8bit_v1 UUT1(.A(a_tv), .B(b_tv), 
+                                 .cin(cin_tv),
+                                 .sum(sum_ov_v1),
+                                 .cout(cout_ov_v1));
+   approximate_adder_8bit_v2 UUT2(.A(a_tv), .B(b_tv), 
+                                 .cin(cin_tv),
+                                 .sum(sum_ov_v2),
+                                 .cout(cout_ov_v2));
+   approximate_adder_8bit_v5 UUT3(.A(a_tv), .B(b_tv), 
+                                 .cin(cin_tv),
+                                 .sum(sum_ov_v5),
+                                 .cout(cout_ov_v5));
+   
    initial begin : maintestbench
-      $display("| a   | b   | sum | cout | expected sum | expected cout |");
+      
+      $display("| version | Initials | sum | cout | expected sum | expected cout |");
 
       // Initials A L
       a = 65;
       b = 76;
       #1;
       {expected_cout, expected_sum} = a + b;
-      $display("| %d | %d | %d |    %d |          %d |            %d |", 
-               a_tv, b_tv, sum_ov, cout_ov, expected_sum, expected_cout);
+      $display("|   v1    |       AL | %d |    %d |          %d |             %d |", 
+               sum_ov_v1, cout_ov_v1, expected_sum, expected_cout);
+      $display("|   v2    |       AL | %d |    %d |          %d |             %d |", 
+               sum_ov_v2, cout_ov_v2, expected_sum, expected_cout);
+      $display("|   v5    |       AL | %d |    %d |          %d |             %d |", 
+               sum_ov_v5, cout_ov_v5, expected_sum, expected_cout);
       
       // Initials G B
       a = 71;
       b = 66;
       #1;
       {expected_cout, expected_sum} = a + b;
-      $display("| %d | %d | %d |    %d |          %d |            %d |", 
-               a_tv, b_tv, sum_ov, cout_ov, expected_sum, expected_cout);
+      $display("|   v1    |       GB | %d |    %d |          %d |             %d |", 
+               sum_ov_v1, cout_ov_v1, expected_sum, expected_cout);
+      $display("|   v2    |       GB | %d |    %d |          %d |             %d |", 
+               sum_ov_v2, cout_ov_v2, expected_sum, expected_cout);
+      $display("|   v5    |       GB | %d |    %d |          %d |             %d |", 
+               sum_ov_v5, cout_ov_v5, expected_sum, expected_cout);
       
       // Initials T I
       a = 84;
       b = 73;
       #1;
       {expected_cout, expected_sum} = a + b;
-      $display("| %d | %d | %d |    %d |          %d |            %d |", 
-               a_tv, b_tv, sum_ov, cout_ov, expected_sum, expected_cout);
+      $display("|   v1    |       TI | %d |    %d |          %d |             %d |", 
+               sum_ov_v1, cout_ov_v1, expected_sum, expected_cout);
+      $display("|   v2    |       TI | %d |    %d |          %d |             %d |", 
+               sum_ov_v2, cout_ov_v2, expected_sum, expected_cout);
+      $display("|   v5    |       TI | %d |    %d |          %d |             %d |", 
+               sum_ov_v5, cout_ov_v5, expected_sum, expected_cout);
       
    end
 endmodule
